1f8a9814f74dacc608287859acdeb840e20bdb0e
himanshukmwt <himanshukumawat8824@gmail.com>
2026-08-29 12:06:35 +0530

    issue solve

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -70,8 +70,9 @@ const Payment = () => {
         config: {
           display: {
             blocks: { upi: { name: 'Pay via UPI', instruments: [
-          { method: 'upi', flows: ['collect'] },
+  { method: 'upi', flows: ['collect'] },
   { method: 'upi', flows: ['qr'] },
+], } },
             sequence: ['block.upi'],
             preferences: { show_default_blocks: false },
           },
