a9029bbe7e3051723a6d00810ca22af7f5e3f44f
himanshukmwt <himanshukumawat8824@gmail.com>
2026-08-29 12:08:18 +0530

    payment updated

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -71,7 +71,7 @@ const Payment = () => {
           display: {
             blocks: { upi: { name: 'Pay via UPI', instruments: [
   { method: 'upi', flows: ['collect'] },
-  { method: 'upi', flows: ['qr'] },
+  // { method: 'upi', flows: ['qr'] },
 ], } },
             sequence: ['block.upi'],
             preferences: { show_default_blocks: false },
